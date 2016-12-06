209b6535a9175ed1cceea22e866af53654b7a962
leranwang <leranwang@126.com>
2016-12-06 15:39:20 +0800

    Add files via upload

---
+++ DOING-DATA-SCIENCE/Ad.Newyorkpaper.R
@@ -0,0 +1,70 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#Author:Maura Fitzgerald
+data1 <- read.csv(url("http://stat.columbia.edu/~rachel/datasets/nyt1.csv"))
+
+#分类
+head(data1)
+data1$agecat <-cut(data1$Age,c(-Inf, 0, 18, 24, 34, 44, 54, 64, Inf))
+
+#yu lan
+summary(data1)
+
+#fen zu
+install.packages("doBy")
+library("doBy")
+siterange <- function(x){c(length(x), min(x), mean(x), max(x))}
+summaryBy(Age~agecat, data =data1, FUN=siterange)
+
+#
+summaryBy(Gender+Signed_In + Impressions + Clicks~agecat,
+          data =data1)
+
+#
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(data1, aex(x=Impressions, fill=agecat))
+        +geom_histogram(binwidth=1)
+ggplot(data1, aex(x=agecat, y= Impressions, fill= agecat))
+        +geom_boxplot()
+
+#
+#
+#
+data1$hasimps <- cut(data1$Impressions, c(-Inf, 0,Inf))
+summaryBy(Clicks~hasimps, data = data1, FUN=siterange)
+ggplot(subset(data1, Impressions >0),aes(x= Clicks/Impressions,
+                colour = agecat)) + geom_density()
+ggplot(subset(data1, Clicks>0),aes(x=Clicks/Impressions,
+        colour = agecat)) + geom_density()
+ggplot(subset(data1,Clicks>0), aes(x = agecat, y = Clicks,
+        fill = agecat)) + geom_boxplot()
+ggplot(subset(data1, Clicks >0),aes(x= Clicks, colour = agecat))
+        + geom_density()
+
+#
+data1$scode[data1$Impressions==0] <- "NoImps"
+data1$scode[data1$Impressions>0] <- "Imps"
+data1$scode[data1$Impressions >0] <- "Clicks"
+
+#
+data1$code <- factor(data1$scode)
+head(data1)
+
+#
+clen <- function(x){c(length(x))}
+etable <- summaryBy(Impressions~scode + Gender +agecat,
+                    data = data1, FUN= clen)
